c4b35b1d8dd31f1c336024882aa08f8f3cd913ee
bilalmohammed261 <126739937+bilalmohammed261@users.noreply.github.com>
2023-11-22 09:27:28 -0500

    added code for updating search value

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,10 +1,23 @@
 // Write your code here
 const SuggestionItem = props => {
-  const {suggestionitem} = props
+  const {suggestionitem, updateSearchResult} = props
   const {id, suggestion} = suggestionitem
   console.log(id)
 
-  return <li>{suggestion}</li>
+  const onClickSuggestion = () => {
+    updateSearchResult(suggestion)
+  }
+
+  return (
+    <li>
+      <p>{suggestion}</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        alt="arrow"
+        onClick={onClickSuggestion}
+      />
+    </li>
+  )
 }
 
 export default SuggestionItem
