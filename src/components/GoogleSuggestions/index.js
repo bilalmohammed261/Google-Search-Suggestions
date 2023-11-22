d141d3278f953891aaff6dc5957f7127225ebf37
bilalmohammed261 <126739937+bilalmohammed261@users.noreply.github.com>
2023-11-22 09:28:57 -0500

    added code for updating search value

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,4 +1,5 @@
 // Write your code here
+// Write your code here
 import {Component} from 'react'
 import SuggestionItem from '../SuggestionItem'
 import './index.css'
@@ -10,6 +11,10 @@ class GoogleSuggestions extends Component {
 
   onChangeInput = event => {
     this.setState({searchInput: event.target.value})
+  }
+
+  updateSearchResult = suggestion => {
+    this.setState({searchInput: suggestion})
   }
 
   render() {
@@ -29,16 +34,21 @@ class GoogleSuggestions extends Component {
         />
         <div className="suggestions-container">
           <input
+            placeholder="Search Google"
             type="search"
             value={searchInput}
             onChange={this.onChangeInput}
           />
-
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+            alt="search icon"
+          />
           <ul>
             {filteredsuggestionsList.map(suggestionitem => (
               <SuggestionItem
                 key={suggestionitem.id}
                 suggestionitem={suggestionitem}
+                updateSearchResult={this.updateSearchResult}
               />
             ))}
           </ul>
