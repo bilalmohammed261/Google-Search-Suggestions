6e5300918fe302887289776ad4738ff246e66eaf
bilalmohammed261 <126739937+bilalmohammed261@users.noreply.github.com>
2023-11-23 19:54:59 -0500

    added button tag and css class names

--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,4 +1,5 @@
-// Write your code here
+import './index.css'
+
 const SuggestionItem = props => {
   const {suggestionitem, updateSearchResult} = props
   const {id, suggestion} = suggestionitem
@@ -9,13 +10,19 @@ const SuggestionItem = props => {
   }
 
   return (
-    <li>
-      <p>{suggestion}</p>
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
-        alt="arrow"
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
         onClick={onClickSuggestion}
-      />
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
     </li>
   )
 }
